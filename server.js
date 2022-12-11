4051a6b5206202a5e5550fdc0b2a4804f234d0ec
Alex Voloshenko <alex.voloshenkoua@gmail.com>
2022-12-11 21:27:48 +0200

    test

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(cors())
 const jsonParser = express.json();
 const filePath = 'data.json';
 
-app.get("/api/users", function(req, res){       
+app.get("/api/users", function(req, res){
     const content = fs.readFileSync(filePath,"utf8");
     const users = JSON.parse(content);
     res.send(users);
 });
-app.get("/api/users/:email", function(req, res){          
-    const email = req.params.email; // получаем email    
+app.get("/api/users/:email", function(req, res){
+    const email = req.params.email; // получаем email
     const content = fs.readFileSync(filePath, "utf8");
-    const users = JSON.parse(content);   
+    const users = JSON.parse(content);
     // находим в массиве пользователя по email
     let user = users.find((user) => user.email === email)
     // отправляем пользователя
@@ -27,38 +27,41 @@ app.get("/api/users/:email", function(req, res){
         res.status(404).send();
     }
 });
-app.post("/api/users", jsonParser, function(req, res){    
-    const newUser = req.body   
-    fs.readFile('data.json', (err, data) => { 
+app.post("/api/users", jsonParser, function(req, res){
+    const newUser = req.body
+    fs.readFile('data.json', (err, data) => {
         if (err) {
-            throw err; 
-        }   
-        const newData = JSON.parse(data);     
-        newData.push(newUser);  
-        fs.writeFile('data.json', JSON.stringify(newData), (err) => { 
-            if (err) throw err; 
+            throw err;
+        }
+        const newData = JSON.parse(data);
+        newData.push(newUser);
+        fs.writeFile('data.json', JSON.stringify(newData), (err) => {
+            if (err) throw err;
             res.send('ok');
-        });  
-    });    
+        });
+    });
 });
 app.post("/api/users/login", jsonParser, function(req, res) {
     const user = req.body;
-    fs.readFile('data.json', (err, data) => { 
+    fs.readFile('data.json', (err, data) => {
         if (err) {
-            throw err; 
-        }   
+            throw err;
+        }
         const newData = JSON.parse(data);
         const userExist =  newData.find(function(elem){
             return elem.email === user.email && elem.password === user.password
-        });    
+        });
         if(userExist) {
             userExist.password = false;
             res.send(userExist)
         } else {
             res.send({text: 'Такого користувача не існує'})
         }
-    });   
+    });
 })
+app.get('/', function(request, response) {
+    response.send('Hello World!');
+  });
 
 app.listen(3000, function(){
     console.log("Сервер ожидает подключения...");
